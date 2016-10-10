9cea5f48cc7cc26c8b2a0323cea83eada17b1dda
PeterMader <peter_mader@gmx.de>
2016-10-10 16:51:17 +0200

    The current color sequence is now adopted by the next one.

--- masterbrain.js
+++ masterbrain.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const colorSelector = document.getElementById('colors')
   const restartButton = document.getElementById('restart')
   const attemptButton = document.getElementById('attempt')
+  const gameWrapper = document.getElementById('game')
+  const messageWrapper = document.getElementById('message')
+  const closeMessageButton = document.getElementById('close-msg')
+  const messageContent = document.getElementById('msg-content')
 
   const FIELD_SIZE = 40
   const MAX_ATTEMPTS = 10
@@ -12,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const IS_CORRECT = '#388e3c'
   const IS_INCLUDED = '#ffeb3b'
   const IS_NOT_INCLUDED = '#f44336'
-  const CURRENT_UNSET = '#bdbdbd'
-  const FUTURE_UNSET = '#e0e0e0'
+  const CURRENT_UNSET = '#9e9e9e'
+  const FUTURE_UNSET = '#bdbdbd'
 
   const game = {}
   game.colorSequence = []
@@ -23,8 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
   game.selectedColor = COLORS[0]
   game.attemptCount = 0
   game.colorButton = null
+  game.isPlaying = false
 
-  // Creates a random color sequence
+  // Create a random color sequence
   game.createColorSequence = () => {
     const cs = game.colorSequence
     let index = 0
@@ -36,6 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
       cs[index] = color
     }
   }
+
+  // Compare the game's color sequence with the user input
   game.compareColorSequences = () => {
     const result = game.result = Array.apply(null, Array(4))
     let colIndex = 0
@@ -53,17 +60,25 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
+
+  // Check whether the current attempt can be submitted
   game.canSubmit = () => {
     return game.currentColorSequence.every((value) => {
       return !!value
     }) && game.currentColorSequence.length === 4
   }
+
+  // Initialize or re-initialize the game
   game.init = () => {
+    game.isPlaying = true
     game.createColorSequence()
     game.history = []
+    game.currentColorSequence = Array.apply(null, Array(4))
     game.attemptCount = 0
     game.draw()
   }
+
+  // Draw the scene
   game.draw = () => {
     // Draw the sequence history
     let lineIndex = 0
@@ -89,15 +104,21 @@ document.addEventListener('DOMContentLoaded', () => {
       ctx.fillRect(colIndex * FIELD_SIZE + 10, game.attemptCount * FIELD_SIZE + 10, FIELD_SIZE - 2, FIELD_SIZE - 2)
     }
   }
+
+  // Display the win mask
   game.win = () => {
-    window.alert('You win!')
+    game.showMessage('You win!', 'Restart the game')
   }
+
+  // Evaluate the current attempt
   game.evaluate = () => {
     game.compareColorSequences()
     if (game.result.every((value) => value === IS_CORRECT)) {
       game.win()
     }
   }
+
+  // Set the field with the <index> to the currently selected color
   game.setField = (index) => {
     const currentIndex = game.currentColorSequence.indexOf(game.selectedColor)
     if (currentIndex !== -1) {
@@ -105,27 +126,54 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     game.currentColorSequence[index] = game.selectedColor
     game.draw()
+    return game
   }
+
+  // Submit the current attempt
   game.submit = () => {
-    if (game.attemptCount >= MAX_ATTEMPTS) {
+    if (!game.isPlaying) {
+      return
+    }
+    if (game.attemptCount >= MAX_ATTEMPTS - 1) {
       game.init()
-      alert('You only have ' + MAX_ATTEMPTS + ' attempts!')
+      game.showMessage('You only have ' + MAX_ATTEMPTS + ' attempts!')
       return
     }
     if (!game.canSubmit()) {
+      game.showMessage('All fields of the current row must be filled!')
       return
     }
     game.evaluate()
     game.history[game.attemptCount] = {
-      colors: game.currentColorSequence,
+      colors: game.currentColorSequence.slice(),
       result: game.result.sort()
     }
     game.attemptCount += 1
-    game.currentColorSequence = Array.apply(null, Array(4))
     game.result = Array.apply(null, Array(4))
     game.draw()
   }
 
+  game.showMessage = (msg, btnText, onClose) => {
+    messageContent.textContent = msg
+    closeMessageButton.textContent = btnText || 'OK'
+    messageWrapper.classList.add('msg-active')
+    gameWrapper.classList.add('faded-out')
+    if (typeof onClose !== 'function') {
+      return
+    }
+    let handler
+    closeMessageButton.addEventListener('click', handler = () => {
+      closeMessageButton.removeEventListener('click', handler)
+      onClose()
+    })
+  }
+
+  game.hideMessage = () => {
+    messageWrapper.classList.remove('msg-active')
+    gameWrapper.classList.remove('faded-out')
+  }
+
+  // Initialize the color buttons
   let colorIndex = 0
   for (; colorIndex < COLORS.length; colorIndex += 1) {
     const colorButton = document.createElement('div')
@@ -149,6 +197,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   canvas.addEventListener('click', (e) => {
+    if (!game.isPlaying) {
+      return
+    }
     const clientRect = canvas.getBoundingClientRect()
     const x = e.clientX - clientRect.left
     const index = Math.floor((x - 10) / FIELD_SIZE)
@@ -158,11 +209,13 @@ document.addEventListener('DOMContentLoaded', () => {
     game.setField(index)
   })
 
+  game.draw()
+
   restartButton.addEventListener('click', game.init)
   attemptButton.addEventListener('click', game.submit)
+  closeMessageButton.addEventListener('click', game.hideMessage)
 
-
-  game.init()
+  game.showMessage('Start the game!', 'OK', game.init)
 
   if (window.DEBUG) {
     window.game = game
